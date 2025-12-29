d9bde73183408165d7d9d4c4038f87d1b57d2767
loken1016 <alexre0812@gmail.com>
2025-12-29 15:36:02 -0500

    prueba de blink

---
+++ blink/src/main.rs
@@ -0,0 +1,17 @@
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_hal::sys::link_patches();
+    let peripherals = Peripherals::take()?;
+
+    let mut led = PinDriver::output(peripherals.pins.gpio8)?;
+
+    loop {
+        led.set_high()?;
+        FreeRtos::delay_ms(1000);
+        led.set_low()?;
+        FreeRtos::delay_ms(1000);
+    }
+}
